30e282f0aa0957c90db68d8bacf0a73512fed59c
Jan Macario <jmacario@gmu.edu>
2025-02-28 12:52:07 -0500

    Adds a check to see whether the plugin options have been saved before attepting to customize the login page based on the plugin options to avoid a php warning.

--- php/login-page.php
+++ php/login-page.php
@@ -6,14 +6,18 @@
 
 
 // add body class to login page if we should hide the standard WordPress login form
-if (get_option('gmuw_miniorange_options')['gmuw_miniorange_settings_disable_login_form']==1) {
+if (get_option('gmuw_miniorange_options')) {
 
-	add_filter('login_body_class', 'gmuw_miniorange_add_login_page_class');
-	function gmuw_miniorange_add_login_page_class($classes) {
+	if (get_option('gmuw_miniorange_options')['gmuw_miniorange_settings_disable_login_form']==1) {
 
-		$classes[] = 'gmuw_hideloginform';
+		add_filter('login_body_class', 'gmuw_miniorange_add_login_page_class');
+		function gmuw_miniorange_add_login_page_class($classes) {
 
-		return $classes;
+			$classes[] = 'gmuw_hideloginform';
+
+			return $classes;
+
+		}
 
 	}
 
